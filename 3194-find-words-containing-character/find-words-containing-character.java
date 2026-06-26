d566fd051323a06b7a91169fb1f68f23cf2b945f
Nandhakumar-18 <bnandhakumar927@gmail.com>
2026-06-26 20:37:28 +0530

    Time: 3 ms (5.25%) | Memory: 46.7 MB (35.46%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.java
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < words.length; i++){
             String str = words[i];
             for(char ch : str.toCharArray()){
-                if(ch == x & !indexes.contains(i)) indexes.add(i);
+                if(ch == x && !indexes.contains(i)) indexes.add(i);
             }
         }
         return indexes;
