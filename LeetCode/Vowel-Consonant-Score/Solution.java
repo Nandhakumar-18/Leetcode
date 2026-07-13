64484cbfbacd6f2b305abb6088558dff5c987036
Nandhakumar-18 <bnandhakumar927@gmail.com>
2026-07-13 20:16:43 +0530

    Add solution for Vowel-Consonant Score

--- LeetCode/Vowel-Consonant-Score/Solution.java
+++ LeetCode/Vowel-Consonant-Score/Solution.java
@@ -1,17 +1,13 @@
 1class Solution {
 2    public int vowelConsonantScore(String s) {
 3        int v = 0, c = 0;
-4
-5        Set<Character> vowels = Set.of(
-6            'a','e','i','o','u',
-7            'A','E','I','O','U'
-8        );
-9        for (char ch : s.toCharArray()) {
-10            if (!Character.isLetter(ch)) continue;
-11            if (vowels.contains(ch)) v++;
-12            else c++;
-13        }
-14        if (c == 0) return 0;
-15        return v / c;
-16    }
-17}
+4        Set<Character> vowels = Set.of('a','e','i','o','u','A','E','I','O','U');
+5        for (char ch : s.toCharArray()) {
+6            if (!Character.isLetter(ch)) continue;
+7            if (vowels.contains(ch)) v++;
+8            else c++;
+9        }
+10        if (c == 0) return 0;
+11        return v / c;
+12    }
+13}
